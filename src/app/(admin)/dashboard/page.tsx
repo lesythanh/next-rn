5538a18103d8c438e3e5ff9df46a3fffebd26e3d
Jay <jay@bradshawgroup.global>
2024-11-09 10:23:29 +0700

    update layout

---
+++ src/app/(admin)/dashboard/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import AdminFooter from '@/components/layout/admin.footer';
+import AdminHeader from '@/components/layout/admin.header';
+import AdminSidebar from '@/components/layout/admin.sidebar';
+import { Layout } from 'antd';
+import React from 'react';
+
+const DashboardPage = () => {
+
+  const { Content } = Layout;
+
+    return (
+      <div>
+        DashboardPage
+      </div>
+    );
+  };
+  
+  export default DashboardPage;
